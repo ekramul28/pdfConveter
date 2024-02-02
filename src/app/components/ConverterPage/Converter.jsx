95254e011056029d960ec310476a037f49da8ff6
Ekramul <mdekramulhassan168@gmail.com>
2024-02-02 18:40:35 +0600

    conveter page added

---
+++ src/app/components/ConverterPage/Converter.jsx
@@ -0,0 +1,17 @@
+import { usePathname } from "next/navigation";
+
+const Converter = ({ File }) => {
+    console.log(File)
+    return (
+        <div className="flex min-h-screen">
+            <div className="w-3/4">
+                ok
+            </div>
+            <div className="w-1/4 ">
+
+            </div>
+        </div>
+    );
+};
+
+export default Converter;
