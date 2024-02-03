2072f464b5606ed299a8ac9c4ef978d4a220dd11
Ekramul <mdekramulhassan168@gmail.com>
2024-02-03 21:21:35 +0600

    image and pdf view web page done

--- src/app/components/ConverterPage/Converter.jsx
+++ src/app/components/ConverterPage/Converter.jsx
@@ -1,11 +1,28 @@
-import { usePathname } from "next/navigation";
+"use client"
+import Image from "next/image";
+import { useState } from "react";
 
 const Converter = ({ File }) => {
-    console.log(File)
+    const [url, seturl] = useState({})
+    const fileReader = new FileReader()
+    fileReader.onload = (e) => {
+        e.preventDefault()
+        const data = fileReader.result
+        seturl(data)
+    }
+    fileReader.readAsDataURL(File)
     return (
         <div className="flex min-h-screen">
             <div className="w-3/4">
-                ok
+                <iframe src={url} width="100%" height="600px"></iframe>
+                {/* <embed src={url} type="application/pdf" width="100%" height="600px" /> */}
+
+                {/* <Image
+                    src={url}
+                    width={400}
+                    height={60}
+                    alt="img">
+                </Image> */}
             </div>
             <div className="w-1/4 ">
 
